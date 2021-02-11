dde720f571497b8062210f22240f8756bc33a861
Lukas Oppermann <oppermann.lukas@gmail.com>
2021-02-11 14:28:19 +0100

    working on scale poc

--- dist/plugin.js
+++ dist/plugin.js
@@ -19,60 +19,85 @@
         width: 300,
         height: 500
     });
+    const createContainer = () => {
+        const frame = figma.createFrame();
+        frame.layoutMode = 'VERTICAL';
+        frame.itemSpacing = 20;
+        frame.paddingTop = 20;
+        frame.paddingRight = 20;
+        frame.paddingBottom = 20;
+        frame.paddingLeft = 20;
+        frame.primaryAxisSizingMode = 'AUTO';
+        frame.counterAxisSizingMode = 'AUTO';
+        //
+        return frame;
+    };
+    const allowedEffectType = ['DROP_SHADOW', 'INNER_SHADOW'];
+    const parseValue = (valueString, i) => {
+        console.log(valueString, i);
+        console.log(parseInt(eval(valueString.replace('#', i)), 10));
+        return parseInt(eval(valueString.replace('#', i)), 10);
+    };
+    // const opacity = parseInt(eval(msg.opacity.replace('$i', i)), 10) / 100
+    const createElevationLayer = (i, layer) => ({
+        // define elevation
+        type: allowedEffectType.includes(layer.effectType) ? layer.effectType : 'DROP_SHADOW',
+        color: {
+            r: 0,
+            g: 0,
+            b: 0,
+            a: parseValue(layer.opacity, i) / 100 // in percent
+        },
+        offset: {
+            x: parseValue(layer.x, i),
+            y: parseValue(layer.y, i)
+        },
+        spread: parseValue(layer.spread, i),
+        radius: parseValue(layer.blur, i),
+        // defaults
+        blendMode: 'NORMAL',
+        visible: true
+    });
+    const createShadowRepresenation = (i, elevation) => {
+        const rect = figma.createRectangle();
+        rect.x = i * 150;
+        rect.resizeWithoutConstraints(320, 120);
+        rect.effects = elevation;
+        rect.cornerRadius = 5;
+        rect.fills = [{ type: 'SOLID', color: { r: 1, g: 1, b: 1 } }];
+        return rect;
+    };
+    const createStyles = (i, elevation, createStyles) => {
+    };
     // Calls to "parent.postMessage" from within the HTML page will trigger this
     // callback. The callback will be passed the "pluginMessage" property of the
     // posted message.
     figma.ui.onmessage = msg => {
         // One way of distinguishing between different types of messages sent from
         // your HTML page is to use an object with a "type" property like this.
-        if (msg.type === 'create-rectangles') {
-            const frame = figma.createFrame();
-            frame.layoutMode = 'VERTICAL';
-            frame.itemSpacing = 20;
-            frame.paddingTop = 20;
-            frame.paddingRight = 20;
-            frame.paddingBottom = 20;
-            frame.paddingLeft = 20;
-            frame.primaryAxisSizingMode = 'AUTO';
-            frame.counterAxisSizingMode = 'AUTO';
+        if (msg.type === 'saveShadows') {
+            // create container
+            const container = createContainer();
             const nodes = [];
             for (let i = 0; i < msg.count; i++) {
-                console.log(msg.opacity.replace('$i', i));
-                console.log(parseInt(eval(msg.opacity.replace('$i', i)), 10));
-                const opacity = parseInt(eval(msg.opacity.replace('$i', i)), 10) / 100;
-                // define elevation
-                const elevation = [{
-                        type: 'DROP_SHADOW',
-                        color: {
-                            r: 0,
-                            g: 0,
-                            b: 0,
-                            a: opacity
-                        },
-                        offset: {
-                            x: msg.x,
-                            y: msg.y
-                        },
-                        spread: msg.spread,
-                        radius: msg.blur,
-                        blendMode: 'NORMAL',
-                        visible: true
-                    }];
-                const rect = figma.createRectangle();
-                rect.x = i * 150;
-                rect.resizeWithoutConstraints(320, 120);
-                rect.effects = elevation;
-                rect.fills = [{ type: 'SOLID', color: { r: 1, g: 1, b: 1 } }];
-                frame.appendChild(rect);
-                nodes.push(rect);
+                // get elevation
+                const elevation = msg.shadowLayers.map(layer => createElevationLayer(i, layer));
+                // create elements
+                const shadowRepresentation = createShadowRepresenation(i, elevation);
+                // append to container
+                container.appendChild(shadowRepresentation);
+                nodes.push(shadowRepresentation);
+                // create styles
+                createStyles(i, elevation, msg.createStyles);
             }
-            figma.currentPage.appendChild(frame);
+            // append & select
+            figma.currentPage.appendChild(container);
             figma.currentPage.selection = nodes;
             figma.viewport.scrollAndZoomIntoView(nodes);
         }
         // Make sure to close the plugin when you're done. Otherwise the plugin will
         // keep running, which shows the cancel button at the bottom of the screen.
-        figma.closePlugin();
+        // figma.closePlugin()
     };
     
     'marker:resolver';
