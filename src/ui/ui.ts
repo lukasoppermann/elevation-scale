99e8b121d94455fd72374bbe28444a7faaf87442
Lukas Oppermann <oppermann.lukas@gmail.com>
2021-02-13 23:00:56 +0100

    working on using webpack

--- src/ui/ui.ts
+++ src/ui/ui.ts
@@ -1,10 +1,10 @@
 // selections
-const elevationLayerTemplate = document.getElementById('elevationLayerTemplate')
+const elevationLayerTemplate = document.getElementById('elevationLayerTemplate') as HTMLTemplateElement
 const sectionElevationSettings = document.querySelector('[data-section="elevationSettings"]')
 const sectionEmptyState = document.querySelector('[data-section="emptyState"]')
 const list = document.getElementById('elevationLayers')
-const count = document.querySelector('[data-property="count"]')
-const createStyles = document.querySelector('[data-property="createStyles"]')
+const count = document.querySelector('[data-property="count"]') as HTMLInputElement
+const createStyles = document.querySelector('[data-property="createStyles"]') as HTMLInputElement
 // events
 onmessage = ({ data = undefined }) => {
   if (data !== undefined && data.pluginMessage !== undefined) {
@@ -75,12 +75,12 @@ document.getElementById('createScale').onclick = () => {
   parent.postMessage({ pluginMessage: { type: 'createScale' } }, '*')
 }
 
-const createShadowLayer = values => {
+const createShadowLayer = (values = {}) => {
   // get clone
-  const clone = elevationLayerTemplate.content.cloneNode(true)
+  const clone = elevationLayerTemplate.content.cloneNode(true) as HTMLElement
   // replace values
   for (const key in values) {
-    clone.querySelector(`[data-property="${key}"]`).value = values[key]
+    (clone.querySelector(`[data-property="${key}"]`) as HTMLInputElement).value = values[key]
   }
   // return layer
   return clone
@@ -91,8 +91,8 @@ document.getElementById('add').onclick = () => {
 }
 
 list.onclick = (e) => {
-  if (e.target.dataset.action === 'deleteItem') {
-    e.target.parentNode.parentNode.remove()
+  if ((e.target as HTMLElement).dataset.action === 'deleteItem') {
+    ((e.target as HTMLElement).parentNode.parentNode as HTMLElement).remove()
   }
 }
 
