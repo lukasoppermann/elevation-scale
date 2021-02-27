247b4f36e05c3a247adaad05785c2a3cf18fb587
Lukas Oppermann <oppermann.lukas@gmail.com>
2021-02-27 21:18:42 +0100

    disable create styles on copy as well

--- src/modules/fixDuplicate.ts
+++ src/modules/fixDuplicate.ts
@@ -3,13 +3,12 @@ import { getContainerData, setContainerData, storeKeys } from './containerStore'
 export default currentContainer => {
   const containerData = getContainerData(currentContainer, storeKeys.ELEVATION_SETTNGS)
   // remove styles if container was duplicated
-  console.log(containerData, currentContainer.id)
   if (containerData.containerId !== null && containerData.containerId !== currentContainer.id) {
     containerData.styles = []
+    containerData.createStyles = false
   }
   // add container id
   containerData.containerId = currentContainer.id
-  console.log('now', containerData)
   // update container data
   setContainerData(currentContainer, storeKeys.ELEVATION_SETTNGS, containerData)
 }
